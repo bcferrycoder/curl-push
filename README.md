9ccfbe2d9df19f4aed4125ff04e680ad6030f4b7
jdw <jdw@bcferrycoder.com>
2015-01-07 00:17:58 +0000

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ curl-push
 =========
 
 Curl script to push app to Cloud Foundry
+
+
+### Requirements
+
+This script uses the [jq](http://stedolan.github.io/jq/) json parser,
+which must be installed and available on $PATH prior to invoked this
+script.
+
+jq is available on all major OS's.
+
+
+### Usage
+
+* Create a zipfile containing the application directory
+* Edit the curl-push.sh script and update the 
---
+++ curl-push.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# this script invokes the Cloud Foundry APIs via curl to push an application
+
+# Customize these
+STACKATO_HOST=https://api.192.168.0.112.xip.io
+USERNAME=jdw
+PASSWORD=jdw
+APPNAME=java-hello
+ZIPFILE=my-app.zip
+
+
+if [ ! -f "$ZIPFILE" ]
+then
+    echo "Application zip file required"
+    echo "Create a zipfile containing the app and manifest, update ZIPFILE above, and try again"
+    exit
+fi
+
+echo "######### /uaa/oauth/token"
+TOKEN=`curl -s -k -H 'AUTHORIZATION: Basic Y2Y6' -d "username=${USERNAME}&password=${PASSWORD}&grant_type=password" ${STACKATO_HOST}/uaa/oauth/token | jq -r .access_token`
+AUTH="Authorization: bearer $TOKEN"
+
+export SPACE=`curl -s -k -H "${AUTH}" ${STACKATO_HOST}/v2/spaces| jq -r ".resources[0].metadata.guid"`
+echo "SPACE: $SPACE"
+
+export DOMAIN=`curl -s -k -H "${AUTH}" ${STACKATO_HOST}/v2/spaces/${SPACE}/domains | jq -r ".resources[0].metadata.guid"`
+echo "DOMAIN: $DOMAIN"
+
+echo "CREATE APP"
+export POST_DATA=$(cat <<EOF
+{"disk_quota":2048,"memory": 512,"name":"${APPNAME}","space_guid":"${SPACE}"}
+EOF
+)
+curl -k -X POST -d "${POST_DATA}" -H "${AUTH}" ${STACKATO_HOST}/v2/apps
+
+
+echo "CREATE ROUTE"
+export POST_DATA=$(cat <<EOF
+{"domain_guid":"${DOMAIN}","host":"${APPNAME}","space_guid":"${SPACE}"}
+EOF
+)
+curl -k -X POST -d "${POST_DATA}" -H "${AUTH}" ${STACKATO_HOST}/v2/routes
+
+
+echo "GET ROUTE"
+export ROUTE=`curl -s -k -H "${AUTH}" ${STACKATO_HOST}/v2/routes | jq -r ".resources[0].metadata.guid"`
+echo "ROUTE: $ROUTE"
+
+echo "GET APP"
+export APP=`curl -s -k -H "${AUTH}" ${STACKATO_HOST}/v2/apps | jq -r ".resources[0].metadata.guid"`
+echo $APP
+
+echo "ASSOCIATE ROUTE"
+curl -k -X PUT -H "${AUTH}" ${STACKATO_HOST}/v2/apps/${APP}/routes/${ROUTE}
+
+echo "UPLOAD BITS"
+
+
+echo curl -H "Expect:" -0 -include -v -k -X PUT -H "${AUTH}" -F 'resources=[]' -F "application=@${ZIPFILE};type=application/binary" ${STACKATO_HOST}/v2/apps/${APP}/bits
+curl -H "Expect:" -0 -include -v -k -X PUT -H "${AUTH}" -F 'resources=[]' -F "application=@${ZIPFILE};type=application/binary" ${STACKATO_HOST}/v2/apps/${APP}/bits
+
+curl -v -k -X PUT -d '{"console":true,"state":"STARTED"}' -H "${AUTH}" ${STACKATO_HOST}/v2/apps/${APP}
